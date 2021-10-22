0e65366d432317baafd6bde888b524d2ad24b6a8
nik <17rolex@gmail.com>
2021-10-22 17:55:20 +0300

    something

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from .models import Record, Car, GPS
+from .models import Record, Car, GPS, Item
 
 
 admin.site.register(Record)
 admin.site.register(Car)
 admin.site.register(GPS)
-
+admin.site.register(Item)
--- mainapp/models.py
+++ mainapp/models.py
@@ -8,12 +8,29 @@ class Car(models.Model):
         db_table = 'car'
 
 
+class Item(models.Model):
+    uuid = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = 'item'
+
+
 class Record(models.Model):
+    class ActionType(models.TextChoices):
+        MOVEMENT = 'MV', 'Movement'
+        LOADING = 'LO', 'Loading'
+        UNLOADING = 'UL', 'Unloading'
+
+    action = models.CharField(max_length=2,
+                              choices=ActionType.choices,
+                              null=True)
+    item = models.ForeignKey(Item,
+                             on_delete=models.PROTECT,
+                             null=True)
     file_name = models.CharField(max_length=255)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-    car = models.ForeignKey(Car, on_delete=models.PROTECT)
-    is_deleted = models.BooleanField(default=False)
+    car = models.ForeignKey(Car, on_delete=models.PROTECT, null=True)
 
     class Meta:
         db_table = 'record'
