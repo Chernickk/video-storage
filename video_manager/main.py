bc2860fe8ab07998316279f30d87af68d5ae271a
nik <17rolex@gmail.com>
2021-12-02 12:12:27 +0300

    add logger, refactor video-manager

--- video_manager/main.py
+++ video_manager/main.py
@@ -1,55 +1,11 @@
-import os
-from datetime import timedelta, datetime
-from time import sleep
-from typing import List
+from datetime import timedelta
 
+from manager.video_manager import VideoManager
 from settings import VIDEO_DIR
-from manager.utils import extract_name, merge_clips, extract_datetime, get_duration
 
 
-def check_upload_progress(files: List[str], path):
-    for file in files:
-        create_dt = datetime.fromtimestamp(os.path.getmtime(os.path.join(path, file)))
-        if create_dt - datetime.now() > timedelta(hours=1):
-            return True
-    return False
-
-
-def delete_old_files(path: str, time_delta: timedelta):
-    for file in os.listdir(path):
-        path_to_file = os.path.join(path, file)
-        if not os.path.isdir(path_to_file):
-            if datetime.fromtimestamp(os.path.getmtime(path_to_file)) < (datetime.now() - time_delta):
-                os.remove(path_to_file)
-        else:
-            delete_old_files(path_to_file, time_delta=time_delta)
-
-
-while True:
-    try:
-        for directory in os.listdir(VIDEO_DIR):
-            files = os.listdir(os.path.join(VIDEO_DIR, directory, 'temp'))
-            files.sort()
-            if check_upload_progress(files, os.path.join(VIDEO_DIR, directory, 'temp')):
-                break
-            camera_names = set([extract_name(file) for file in files])
-            for name in camera_names:
-                named_clips = [file for file in files if extract_name(file) == name]
-                while named_clips:
-                    files_to_merge = [named_clips.pop(0)]
-                    while named_clips:
-                        if (extract_datetime(named_clips[0]) - (
-                                extract_datetime(files_to_merge[-1]) + get_duration(files_to_merge[-1]))) < timedelta(
-                                minutes=10):
-                            files_to_merge.append(named_clips.pop(0))
-                        else:
-                            break
-                    merge_clips(files_to_merge, os.path.join(VIDEO_DIR, directory, 'temp'),
-                                os.path.join(VIDEO_DIR, directory))
-
-        delete_old_files(VIDEO_DIR, timedelta(days=60))
-
-    except Exception as error:
-        print(error)
-
-    sleep(100000)
+if __name__ == '__main__':
+    video_manager = VideoManager(video_path=VIDEO_DIR,
+                                 delete_old=False,
+                                 interval=timedelta(minutes=30))
+    video_manager.start()
---
+++ video_manager/manager/video_manager.py
@@ -0,0 +1,72 @@
+import os
+from datetime import timedelta, datetime
+from threading import Thread
+from time import sleep
+from typing import List
+
+from manager.utils import extract_name, extract_datetime, get_duration, merge_clips
+from logs.logger import logger
+
+
+class VideoManager(Thread):
+    def __init__(self, video_path: str, delete_old: bool, interval: timedelta):
+        self.video_path = video_path
+        self.delete_old = delete_old
+        self.interval = interval.total_seconds()
+        super().__init__()
+
+    def check_upload_progress(self, files: List[str], path) -> bool:
+        for file in files:
+            create_dt = datetime.fromtimestamp(os.path.getmtime(os.path.join(path, file)))
+            if create_dt - datetime.now() > timedelta(hours=1):
+                return True
+        return False
+
+    def delete_old_files(self, path: str, time_delta: timedelta) -> None:
+        deleted = False
+        for file in os.listdir(path):
+            path_to_file = os.path.join(path, file)
+            if not os.path.isdir(path_to_file):
+                if datetime.fromtimestamp(os.path.getmtime(path_to_file)) < (datetime.now() - time_delta):
+                    os.remove(path_to_file)
+                    deleted = True
+            else:
+                self.delete_old_files(path_to_file, time_delta=time_delta)
+
+        if deleted:
+            logger.info('old files has been deleted')
+
+    def merge_closest_clips(self, directory: str, files: List[str]) -> None:
+        camera_names = set([extract_name(file) for file in files])
+        for name in camera_names:
+            named_clips = [file for file in files if extract_name(file) == name]
+            while named_clips:
+                files_to_merge = [named_clips.pop(0)]
+                while named_clips:
+                    next_clip_start_time = extract_datetime(named_clips[0])
+                    last_clip_end_time = extract_datetime(files_to_merge[-1]) + get_duration(files_to_merge[-1])
+                    if (next_clip_start_time - last_clip_end_time) < timedelta(minutes=10):
+                        files_to_merge.append(named_clips.pop(0))
+                    else:
+                        break
+                result_name = merge_clips(files_to_merge, os.path.join(self.video_path, directory, 'temp'),
+                                          os.path.join(self.video_path, directory))
+                logger.info(f'File created: {result_name}')
+
+    def run(self):
+        while True:
+            try:
+                for directory in os.listdir(self.video_path):
+                    files = os.listdir(os.path.join(self.video_path, directory, 'temp'))
+                    files.sort()
+                    if self.check_upload_progress(files, os.path.join(self.video_path, directory, 'temp')):
+                        break
+                    self.merge_closest_clips(directory, files)
+                if self.delete_old:
+                    self.delete_old_files(self.video_path, timedelta(days=60))
+
+            except Exception as error:
+                logger.exception(f'{error}')
+
+            finally:
+                sleep(self.interval)
